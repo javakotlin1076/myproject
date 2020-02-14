05ebd962f53407576e1db8a5e9e7452d409ec093
Bobo Tsai <ch0911443@gmail.com>
2020-02-14 09:48:57 +0800

    Initial commit

---
+++ src/main/java/com/bobo/Hello.java
@@ -0,0 +1,11 @@
+package com.bobo;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class Hello {
+    public static void main(String[] args) {
+//       System.out.println("Hello world");
+        Person p=new Person();
+        p.hello();
+    }
+}
